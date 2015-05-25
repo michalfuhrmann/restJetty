1fe431b412f8a83ae5472050fad4175343186bdf
Michal Fuhrmann <torosmg@gmail.com>
2015-05-25 22:37:18 +0200

    start of etity design

---
+++ src/main/java/com/sherman/entity/Language.java
@@ -0,0 +1,7 @@
+package com.sherman.entity;
+
+
+public enum Language {
+
+    ENGLISH,POISH,GERMAN
+}
---
+++ src/main/java/com/sherman/entity/Word.java
@@ -0,0 +1,14 @@
+package com.sherman.entity;
+
+import java.util.List;
+
+public class Word {
+
+    private String text;
+
+    private Language language;
+
+    private List<Word> translations;
+    
+
+}
