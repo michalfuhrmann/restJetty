9a136e0eefc24bc804ae6e72d2bd1ae2470e401e
Michal Fuhrmann <michal.fuhrmann@misys.com>
2015-05-25 16:53:55 +0200

    test dir added

---
+++ src/test/java/com/sherman/rest/MyResourceTest.java
@@ -0,0 +1,16 @@
+package com.sherman.rest;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class MyResourceTest {
+
+
+    @Test
+    public void test() {
+        Assert.fail();
+    }
+}
